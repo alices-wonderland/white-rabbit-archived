52570cf2dbf9e06c6d6123bf6b9e30e2a208c0c1
Ukonn Ra <ukonnra@outlook.com>
2021-12-27 23:29:39 +0800

    refactor: add module-info for endpoint.graphql

---
+++ white-rabbit-endpoint-graphql/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module whiterabbit.endpoint.graphql {
+  requires spring.boot;
+  requires spring.boot.autoconfigure;
+  requires lombok;
+  requires whiterabbit.core;
+  requires spring.context;
+  requires java.transaction;
+  requires spring.data.commons;
+  requires spring.core;
+  requires reactor.core;
+  requires com.graphqljava;
+  requires graphql.java.extended.scalars;
+  requires spring.graphql;
+}
